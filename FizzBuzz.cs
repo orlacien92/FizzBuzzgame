e5a2c87c13784716c805f202559769987fe1cc79
Krzysztof Orzeł <krzysztof.orzel92@gmail.com>
2023-01-30 19:09:34 +0100

    Add all project files

---
+++ FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class FizzBuzz
+    {
+        
+   
+        public string Calculate(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                {
+                return "FizzBuzz";
+                }
+            if (number % 3 == 0)
+                {
+                return "Fizz";
+                }
+            if (number % 5 == 0)
+                {
+                return "Buzz";
+                }
+
+            return number.ToString();
+        }
+
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new FizzBuzz();
+
+            Console.WriteLine("Witaj w grze FizzBuzz!");
+
+            while (true)
+            {
+                Console.WriteLine("Podaj swoją liczbę: ");
+
+                var numberOfUser = GetNumber();
+
+                Console.WriteLine(number.Calculate(numberOfUser));
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if(!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową liczbę, spróbuj jeszcze raz!");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+    }
+}
